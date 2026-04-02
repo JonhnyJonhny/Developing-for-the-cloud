ea9179e30b59b6e1748b0ba3faa6a07fef6e4aa1
JonhnyJonhny <pkone731@gmail.com>
2026-04-02 18:04:48 +0700

    Fix database not deploying

--- Backend/database.tf
+++ Backend/database.tf
@@ -11,7 +11,10 @@ resource "aws_db_instance" "Budget_app_db" {
   db_name = "Budget_app_db_instance"
   username = var.db_username
   password = var.db_password
+<<<<<<< HEAD
   parameter_group_name = "default.mysql8.0"
+=======
+>>>>>>> 217a3a3 (Fix database not deploying)
   skip_final_snapshot = true
   db_subnet_group_name = aws_db_subnet_group.db_subnet.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]

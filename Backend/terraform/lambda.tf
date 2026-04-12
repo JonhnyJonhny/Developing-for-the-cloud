fd25459872a50af3dc234820145ff423c2c2e406
JonhnyJonhny <pkone731@gmail.com>
2026-04-12 23:09:47 +0700

    bug fixes

--- Backend/terraform/lambda.tf
+++ Backend/terraform/lambda.tf
@@ -153,7 +153,7 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.private.id]
+  route_table_ids   = [aws_route_table.private1.id, aws_route_table.private2.id]
   tags = { Name = "s3-endpoint" }
 }
 
